5b897e0c1f76e616383d20e8126783b367dc5718
wss711 <281967428@qq.com>
2020-02-14 20:05:15 +0800

    light: 0.0.1 network 3

--- app/src/main/java/com/gr/light/patterns/singleton/HungrySingleton.java
+++ app/src/main/java/com/gr/light/patterns/singleton/HungrySingleton.java
@@ -10,6 +10,7 @@ public class HungrySingleton {
      private static HungrySingleton instance = new HungrySingleton();
 
      private HungrySingleton(){
+
      }
 
     public static HungrySingleton getInstance() {
