b7cd85856460205f7037a265cd1c0cbe5f626cfb
wss711 <281967428@qq.com>
2020-02-24 01:22:05 +0800

    Light 0.0.1 Architecture 09

--- mvcnote/src/main/java/com/gr/mvcnote/MainActivity.java
+++ mvcnote/src/main/java/com/gr/mvcnote/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 你妈的
+        // 我去你妈的
     }
 }
